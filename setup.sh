bb76fe1ec1bee05dae79fc8f7380a3201688ac38
Tony Young <tony@rfw.name>
2013-09-10 15:19:46 +1200

    forward with $http_host

--- setup.sh
+++ setup.sh
@@ -5,3 +5,12 @@ popd
 pushd cerebro
 python setup.py develop
 popd
+
+echo "Installing global Node.js dependencies..."
+npm install -g uglify-js less bower
+
+echo "Setting up Postgres for the first time..."
+createdb -hlocalhost cerebro-development
+
+echo "Initializing Cerebro database..."
+initialize_cerebro_db cerebro/development.ini

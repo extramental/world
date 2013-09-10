edeb3c26fd0deef1ab6588a7c258734e1f339158
Tony Young <tony@rfw.name>
2013-09-10 15:45:52 +1200

    sudo npm install

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ python setup.py develop
 popd
 
 echo "Installing global Node.js dependencies..."
-npm install -g uglify-js less bower
+sudo npm install -g uglify-js less bower
 
 echo "Setting up Postgres for the first time..."
 createdb -hlocalhost cerebro-development
